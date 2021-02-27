f14df70e4395a6f0184c09de5aefd2919eda1906
Michal <koza41409@gmail.com>
2021-02-27 23:03:17 +0100

    v5

--- installer.sh
+++ installer.sh
@@ -2,5 +2,3 @@
 echo "Instaluję... "
 cd /home/kali/Desktop/METASPLOIT-BY-KOZIOL
 git clone https://github.com/trustedsec/unicorn.git
-sudo apt install -y default-jdk
-sudo apt-get install zipalign
---
+++ koziol.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+# MAIN MENU
+echo "
+   ____________  _______ ______  _____  __  __ _____ ____ _______  __ 
+  / __/ ___/ _ \/  _/ _ /_  __/ / _ \ \/ / / //_/ _ /_  /<  / __ \/ / 
+ _\ \/ /__/ , __/ // ___// /   / _  |\  / / ,< / // // /_/ / /_/ / /__
+/___/\___/_/|_/___/_/   /_/   /____/ /_/ /_/|_|\___//___/_/\____/____/
+
+"
+echo "IF YOU CAN'T FIND VULNERABILITY YOU MUST MAKE IT !!!"
+echo "Which option do you choose ?"
+echo -e "1. METASPLOIT LOW DETACTION WINDOWS PAYLOAD \n2. METASPLOIT EXE PAYLOAD INJECT"
+read ODPOWIEDZ
+if [ $ODPOWIEDZ == "1" ]
+then
+  echo "Uruchamiam wybrany skrypt... "
+  sleep 3
+  cd /home/kali/Desktop/METASPLOIT-BY-KOZIOL/metasploit
+  sudo ./metasploit_bat_shell.sh
+fi
+if [ $ODPOWIEDZ == "2" ]
+then
+  echo "Uruchamiam wybrany skrypt... "
+  cd /home/kali/Desktop/METASPLOIT-BY-KOZIOL/metasploit
+  sudo ./metasploit_exe_injection.sh
+fi
