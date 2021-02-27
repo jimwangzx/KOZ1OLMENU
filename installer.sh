02dfe41223f500cb842215a838ae26461466a564
Michal <koza41409@gmail.com>
2021-02-27 23:25:38 +0100

    update

--- installer.sh
+++ installer.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 echo "Instaluję... "
+sudo chmod +x /home/kali/Desktop/METASPLOIT-BY-KOZIOL/metasploit/metasploit_exe_injection.sh
+sudo chmod +x /home/kali/Desktop/METASPLOIT-BY-KOZIOL/metasploit/metasploit_bat_shell.sh
 cd /home/kali/Desktop/METASPLOIT-BY-KOZIOL
 git clone https://github.com/trustedsec/unicorn.git
